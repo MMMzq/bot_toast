a93fa33a3b436857ae1050dfce734282cb0de112
m <vivask770@163.com>
2019-09-23 22:41:35 +0800

    Modify the initialization method

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,13 +17,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BotToast Demo',
-      navigatorObservers: [BotToastNavigatorObserver()], 
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BotToastInit(
+      child: MaterialApp(
+        title: 'BotToast Demo',
+        navigatorObservers: [BotToastNavigatorObserver()],
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: EnterPage(),
       ),
-      home: BotToastInit(child: EnterPage()),
     );
   }
 }
