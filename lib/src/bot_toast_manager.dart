88fa95fd990b1a2b2f339719427d72e51e34207c
jonmo <vivask770@163.com>
2020-08-26 14:03:18 +0800

    fix:  The bug that `ProxyDispose.disposeCallback` callback cannot be triggered

--- lib/src/bot_toast_manager.dart
+++ lib/src/bot_toast_manager.dart
@@ -21,6 +21,8 @@ class BotToastManager extends StatefulWidget {
 class BotToastManagerState extends State<BotToastManager> {
   final Map<String, Map<UniqueKey, Widget>> _map = {};
 
+  final Set<UniqueKey> _pending = Set<UniqueKey>();
+
   List<Widget> get _children => _map.values.fold([], (value, items) {
         return value..addAll(items.values);
       });
@@ -29,6 +31,14 @@ class BotToastManagerState extends State<BotToastManager> {
     safeRun(() {
       _map[groupKey] ??= {};
       final uniqueKey = UniqueKey();
+
+      widget = ProxyInitState(
+        initStateCallback: () {
+          _pending.remove(key);
+        },
+        child: widget,
+      );
+
       widget = ProxyDispose(
         key: uniqueKey,
         child: widget,
@@ -37,27 +47,52 @@ class BotToastManagerState extends State<BotToastManager> {
         },
       );
       _map[groupKey][key] = widget;
+      _pending.add(key);
       _update();
     });
   }
 
   void remove(String groupKey, UniqueKey key) {
     safeRun(() {
-      _map[groupKey]?.remove(key);
-      _update();
+      if (_pending.contains(key)) {
+        //首桢渲染完成之前,就被删除,需要确保ProxyDispose被安装,因此要放到下一帧进行删除
+        return remove(groupKey, key);
+      } else {
+        _map[groupKey]?.remove(key);
+        _update();
+      }
     });
   }
 
   void removeAll(String groupKey) {
     safeRun(() {
-      _map[groupKey]?.clear();
+      if (_map[groupKey] == null) {
+        return;
+      }
+
+      _map[groupKey].removeWhere((key, _) => !_pending.contains(key));
       _update();
+
+      if (_map[groupKey].isNotEmpty) {
+        _map[groupKey].forEach((key, value) {
+          return remove(groupKey, key);
+        });
+      }
     });
   }
 
   void cleanAll() {
     safeRun(() {
-      _map.clear();
+      _map.forEach((groupKey, value) {
+        assert(value != null);
+        value.removeWhere((key, _) => !_pending.contains(key));
+
+        if (value.isNotEmpty) {
+          value.forEach((key, value) {
+            return remove(groupKey, key);
+          });
+        }
+      });
       _update();
     });
   }
